1dd049c7238620c4bcbe16b5fd036f854948b6be
Lucky Adogun <meetluckyadogun@gmail.com>
2020-10-03 17:09:58 +0100

    sticky added2

--- components/elements/MobileNotificationBar.vue
+++ components/elements/MobileNotificationBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="stickynotif" class="bg-theme-primary h-auto w-full px-10 py-6 leading-none text-white text-md text-center rounded-t-lg">
+    <div id="stickynotif" class="bg-theme-primary h-auto w-full px-10 py-6 leading-none text-white text-sm text-center rounded-t-lg">
         <h3>
             We are launching to the world in 3-2-1...<br>
             <strong>Gain Early Access</strong> to the platform and connect with our founders.
